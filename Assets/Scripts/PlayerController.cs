305e8529636a58aaec56b5d8e824788b4cd21fd0
TTunnock <107088312+TTunnock@users.noreply.github.com>
2022-06-08 11:49:04 +1000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    //float values require f after the value
+    Rigidbody rb;
+    public float speed = 1.0f;
+    void Start()
+    {
+        //Gets the rigidbody component attached to this game object
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        //movement controls
+        //Store the horizontal axis value in a float
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the vertical axis value in a float
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Create a new vector 3 based on the horizontal and vertical values
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //Add force to our rigidbody from our movement vector times our speed
+        rb.AddForce(movement * speed);
+
+
+    }
+}

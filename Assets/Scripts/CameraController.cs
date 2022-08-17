73f84b45318a74aa901915b59b010a062299c1f5
TTunnock <107088312+TTunnock@users.noreply.github.com>
2022-08-17 10:40:10 +1000

    A2 submission

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,9 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+   
 
-
-    void Start()
+        void Start()
     {
         //set the offset to the cameras position minus the players position
         offset = transform.position - player.transform.position;

acd2b4d37dffa4a24bf404849815205fcbfd79eb
TTunnock <107088312+TTunnock@users.noreply.github.com>
2022-08-22 16:38:20 +1000

    22/08 v2

--- Assets/Scripts/RotatingObjects.cs
+++ Assets/Scripts/RotatingObjects.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class RotatingObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float waitTime = 5;
+    public float speed = 10;
+    bool rotated = false;
+    Vector3 startRotation;
+    public Vector3 toRotation = new Vector3(0, 90, 0);
+
+    private void Start()
     {
-        
+        startRotation = transform.eulerAngles;
+        StartCoroutine(Rotate());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Rotate()
     {
-        
+        Vector3 newRot = rotated ? startRotation : toRotation;
+        var toAngle = Quaternion.Euler(newRot);
+        while (transform.rotation != toAngle)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toAngle, speed * Time.deltaTime);
+            yield return null;
+        }
+        yield return new WaitForSeconds(waitTime);
+        rotated = !rotated;
+        StartCoroutine(Rotate());
     }
 }
